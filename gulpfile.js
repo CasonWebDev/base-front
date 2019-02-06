136e860d8106569308166f8db533611aa661beca
João Cason <joao.cason@alamo.br>
2019-02-06 11:37:03 -0200

    v2

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
-    return gulp.src('./assets/sass/**/*.sass')
-    //.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    return gulp.src([
+        './node_modules/animate.css/animate.css',
+        './node_modules/normalize.css/normalize.css',
+        './assets/sass/**/*.*'
+    ], { base: 'themes' })
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(concat('geral.css'))
+    .pipe(uglifycss({
+        "uglyComments": true
+    }))
+    .pipe(sourcemaps.write('maps'))
     .pipe(gulp.dest('./assets/css'));
 });
 
@@ -14,14 +24,18 @@ gulp.task('scripts', function() {
         './bower_components/jquery/dist/jquery.min.js',
         './bower_components/fancybox/dist/jquery.fancybox.min.js',
         './bower_components/jquery-ui/jquery-ui.min.js',
-        './bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
-      ])
-      .pipe(concat('plugins.js'))
+        './node_modules/scroll-out/dist/scroll-out.min.js',
+        './assets/js/scripts.js',
+      ], { base: 'themes'})
+      .pipe(sourcemaps.init())
+      .pipe(concat('geral.js'))
+      .pipe(sourcemaps.write('maps'))
       .pipe(gulp.dest('./assets/js/'));
   });
 
-gulp.task('sass:watch', function () {
-    gulp.watch('./assets/sass/**/*.sass', ['sass']);
+gulp.task('assets:watch', function () {
+    gulp.watch('./assets/sass/**/*.*', ['sass']);
+    gulp.watch('./assets/js/*', ['scripts']);
 });
 
-gulp.task('default', ['sass', 'sass:watch']);
+gulp.task('default', ['sass', 'scripts', 'assets:watch']);
